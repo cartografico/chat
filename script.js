de638c64f80302e84aba0aea0111e7e47ca2e110
cartografico <camaldonadoc@unal.edu.co>
2020-12-13 15:22:40 -0500

    print function updated. Html code convert into a $var

--- script.js
+++ script.js
@@ -1,54 +1,50 @@
 
 //Para el envio del mensaje
 
+$("form").on("submit",function (e) {
+    e.preventDefault();
 
-    $("form").on("submit",function (e) {
-        e.preventDefault();
-
-        const inputValue = $('.messageBox').val()
-
-        let element = $(`<div class="msgBox">
-        <div class="picUser"></div>
-        <div class="content">
-            <div class="data">
-                <div class="name">Jhonny Menomnic</div>
-                <div class="timeStamp"> 11:05</div>
-            </div>
-            <div class="text"> 
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                Culpa possimus earum ipsum optio? Earum, enim? Ab dolorem qui 
-                architecto repellendus mollitia maiores culpa! Tempore modi natus harum ratione nisi vel.
-            </div>
-        </div>`);
-
-        
-        $(".wrap").append(element);
-    
+    let inputValue = $('.messageBox').val()
+    if (inputValue.length > 0){
 
 //Arreglo para mensajes
 
-// const arrayMsg=[
-//     {
-//     user:"Thomas Anderson",
-//      pic:"url",
-//     time: "14:30",
-//     text:inputValue,
-//     }
-// ];
+    const arrayMsg=[
+        {
+        user:"Thomas Anderson",
+        pic:"url",
+        time: "14:30",
+        text:inputValue,
+        }
+    ];
 
 
-// //Imprimir mensajes
-// const msgContainer = document.querySelector(".msgBox");
+    //Imprimir mensajes
+    const msgContainer = $(".wrap");
 
-// function printMessage(){
-//     arrayMsg.forEach(element => {
-//         msgContainer.innerHTML +=`<div class"msgBox"></div>`;
-//     }
-// )};
+    function printMessage(){
+        arrayMsg.forEach(element => {
+        const msgFormat= $(`<div class="msgBox">
+                        <div class="picUser"></div>
+                        <div class="content">
+                            <div class="data">
+                                <div class="name">${element.user}</div>
+                                <div class="timeStamp"> 11:05</div>
+                        </div>
+                        <div class="text"> 
+                            ${element.text}
+                        </div>
+                    </div>
+                </div>`)
+  
+        msgContainer.append(msgFormat);
+        }
+    )};
 
-// printMessage();
+        printMessage();
 
-
+        let = $('.messageBox').val("");
+    }
 });
 
 
