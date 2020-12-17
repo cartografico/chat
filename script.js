ad010ad8c494a21a849dc13914d119dda84a66ff
cartografico <camaldonadoc@unal.edu.co>
2020-12-17 14:42:33 -0500

    [Feature]: array get print function

--- script.js
+++ script.js
@@ -1,50 +1,68 @@
-
 //Para el envio del mensaje
 
-$("form").on("submit",function (e) {
-    e.preventDefault();
+$("form").on("submit", function (e) {
+  e.preventDefault();
 
-    let inputValue = $('.messageBox').val()
-    if (inputValue.length > 0){
+  let inputValue = $(".messageBox").val();
+  if (inputValue.length > 0) {
+    //Arreglo para mensajes
 
-//Arreglo para mensajes
-
-    const arrayMsg=[
+    const arrayMsg = [
         {
-        user:"Thomas Anderson",
-        pic:"url",
+        user: "Thomas Anderson",
+        pic: "url",
         time: "14:30",
-        text:inputValue,
-        }
+        text: inputValue,
+        },
     ];
-
 
     //Imprimir mensajes
     const msgContainer = $(".wrap");
 
-    function printMessage(){
-        arrayMsg.forEach(element => {
-        const msgFormat= $(`<div class="msgBox">
-                        <div class="picUser"></div>
-                        <div class="content">
-                            <div class="data">
-                                <div class="name">${element.user}</div>
-                                <div class="timeStamp"> 11:05</div>
-                        </div>
-                        <div class="text"> 
-                            ${element.text}
-                        </div>
-                    </div>
-                </div>`)
-  
+    function printMessage() {
+        arrayMsg.forEach((element) => {
+            const msgFormat = `<div class="msgBox">
+                                <div class="picUser"></div>
+                                <div class="content">
+                                    <div class="data">
+                                        <div class="name">${element.user}</div>
+                                        <div class="timeStamp"> 11:05</div>
+                                    </div>
+                                    <div class="text">${element.text}</div>
+                                </div>
+                            </div>`;
+
         msgContainer.append(msgFormat);
-        }
-    )};
-
-        printMessage();
-
-        let = $('.messageBox').val("");
+        });
     }
+
+    printMessage();
+
+    let = $(".messageBox").val("");
+
+    } else {
+        const msgContainer = $(".wrap");
+        $.get("https://jsonplaceholder.typicode.com/comments", function (respuesta) {
+            console.log(respuesta);
+
+            respuesta.forEach((element) => {
+                const msgFormat = `<div class="msgBox">
+                                <div class="picUser"></div>
+                                <div class="content">
+                                    <div class="data">
+                                        <div class="name">${element.name}</div>
+                                        <div class="timeStamp"> 11:05</div>
+                                </div>
+                                <div class="text"> 
+                                    ${element.body}
+                                </div>
+                            </div>
+                        </div>`;
+        
+                msgContainer.append(msgFormat);
+                });
+        });
+    }   
 });
 
 
